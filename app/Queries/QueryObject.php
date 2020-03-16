2d063b7fd9da2e79815632e4e9b42ef2d77a41d0
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-03-17 00:00:24 +0100

    Updated queries

--- app/Queries/QueryObject.php
+++ app/Queries/QueryObject.php
@@ -2,15 +2,12 @@
 
 namespace App\Queries;
 
-use Illuminate\Http\Request;
-
 abstract class QueryObject
 {
     /**
      * Handle the incoming request to this query object.
      *
-     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    abstract public function __invoke(Request $request);
+    abstract public function __invoke();
 }

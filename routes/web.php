11c62938cdb2977f8919a1ef8e3e2afe27a8eeb0
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-07-11 17:21:23 +0200

    Remove debug route comment

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/import', function () {
-//     $files = Storage::files('input');
-
-//     foreach ($files as $path) {
-//         $service = Str::of($path)->after('SCHEDA UNICA GESTIONE UTENTI-')->before('_rev');
-//         Log::channel('import')->info('Processing service input file.', ['filename' => $path]);
-
-//         (new BilancioSocialeImport)->for($service)->import($path);
-//     }
-// });
-
 Route::get('dashboard', DashboardController::class)->name('dashboard');
 Route::resource('imports', ImportController::class)->only('index', 'create', 'store');
 Route::resource('queries', QueryController::class)->only('index', 'show');

5aaad289ba0b941c4e6e39e382c871479796756e
Davide Marchetti <davide.marchetti.mail@gmail.com>
2020-10-11 17:37:50 +0200

    Add redirect route to dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::redirect('/', 'dashboard');
 Route::get('dashboard', DashboardController::class)->name('dashboard');
 Route::resource('imports', ImportController::class)->only('index', 'create', 'store');
 Route::resource('queries', QueryController::class)->only('index', 'show');
